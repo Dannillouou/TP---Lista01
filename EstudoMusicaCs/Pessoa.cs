f89a1830aa82e6fc7b3b86bf43dc9467567172da
Dannillouou <danielalmemiranda@gmail.com>
2022-04-28 20:55:29 -0300

    Criação de projeto e alteração de Pessoa.cs
    Todos os arquivos foram encapsulados em uma pasta de projeto para permitir a compilação com dotnet.

---
+++ EstudoMusicaCs/Pessoa.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace EstudoMusica{
+    
+    public abstract class Pessoa{
+        Pessoa(){
+
+        }
+        protected string Nome{ 
+            get{
+                return Nome;
+            } 
+            private set{
+
+            }
+        }
+        protected int CPF{ 
+            get{
+                return CPF;
+            }
+            private set{
+
+            }//mexa nisso depois
+
+        }
+    }
+}
